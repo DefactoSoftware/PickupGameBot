d7beda9356670caa9275a838b8311b307a14b124
Marcel Horlings <marcel.horlings@gmail.com>
2016-05-13 14:42:54 +0200

    add error message and try catch all incomming messages

---
+++ lib/pickup_bot/commands/error.rb
@@ -0,0 +1,27 @@
+module Commands
+  class Error
+    def self.run(telegram_bot, message, exception)
+      new(telegram_bot, message).run
+    end
+
+    def initialize(telegram_bot, message)
+      @telegram_bot = telegram_bot
+      @message = message
+    end
+
+    def run
+      telegram_bot.api.send_message(
+        chat_id: message.chat.id,
+        text: I18n.t('bot.command_does_not_exist', username: username)
+      )
+    end
+
+    private
+
+    attr_reader :telegram_bot, :message
+
+    def username
+      message.from.username || message.from.first_name
+    end
+  end
+end
