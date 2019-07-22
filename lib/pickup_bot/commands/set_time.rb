0d996c1bccd79b2c9f1f571fc30e124b771b9a2c
FatboyPunk <marcel.horlings@gmail.com>
2019-07-22 18:54:39 +0200

    use chronic to return date times according to the given string

--- lib/pickup_bot/commands/set_time.rb
+++ lib/pickup_bot/commands/set_time.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module PickupBot::Commands
   class SetTime
     def self.run(telegram_bot, message)
@@ -10,16 +12,12 @@ module PickupBot::Commands
     end
 
     def run
-      if game_exists? && current_game.update(datetime: datetime)
-          telegram_bot.api.send_message(
-            chat_id: message.chat.id,
-            text: I18n.t("bot.date")
-          )
-        end
-      else
+      return no_game_message unless game_exists?
+
+      if current_game.update(datetime: datetime)
         telegram_bot.api.send_message(
           chat_id: message.chat.id,
-          text: I18n.t("bot.no_game", username: username)
+          text: I18n.t('bot.date', date: datetime)
         )
       end
     end
@@ -27,6 +25,13 @@ module PickupBot::Commands
     private
 
     attr_reader :telegram_bot, :message
+
+    def no_game_message(_message)
+      telegram_bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: I18n.t('bot.no_game', username: username)
+      )
+    end
 
     def game_exists?
       !!current_game
@@ -40,7 +45,7 @@ module PickupBot::Commands
       if current_game.required_players > 0
         "#{current_game.players.count} / #{current_game.required_players}"
       else
-        "#{current_game.players.count}"
+        current_game.players.count.to_s
       end
     end
 
@@ -49,13 +54,13 @@ module PickupBot::Commands
     end
 
     def game_date?
-       current_game.blank?
+      current_game.blank?
     end
 
     def datetime
-      @message.text
-        .then { |x| x.split(" ", 2).last.strip }
-        .then { |x| Chronic.parse(x) }
+      @datetime ||= @message.text
+                            .then { |x| x.split(' ', 2).last.strip }
+                            .then { |x| Chronic.parse(x) }
     end
   end
 end
--- spec/lib/pickup_bot/commands/set_time_spec.rb
+++ spec/lib/pickup_bot/commands/set_time_spec.rb
@@ -1,8 +1,10 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 require 'telegram/bot'
 require 'pickup_bot'
 
-feature "sit time to a game" do
+feature 'sit time to a game' do
   let(:bot) { Telegram::Bot::Client.new('fake-token') }
   let(:pickup_bot) { PickupBot.new(bot) }
   let(:telegram_user) { Telegram::Bot::Types::User.new(user_params) }
@@ -10,66 +12,63 @@ feature "sit time to a game" do
   let(:existing_game) { Game.new(chat_id: fake_chat_id) }
 
   before :each do
-    stub_request(:any, /api.telegram.org/).to_return(status: 200, body:"[]", :headers => {})
+    stub_request(:any, /api.telegram.org/).to_return(status: 200, body: '[]', headers: {})
   end
 
-  context "no game currently exists" do
-    scenario "player tries to add a time" do
+  context 'no game currently exists' do
+    scenario 'player tries to add a time' do
       message = Telegram::Bot::Types::Message.new(message_params('/set_time 06:00'))
 
       pickup_bot.run(message)
 
-      expect(a_request(:post, "https://api.telegram.org/botfake-token/sendMessage").
-              with(body: {
-                "chat_id" => "123",
-                "text" => I18n.t(
-                  "bot.no_game",
-                  username: user_params[:username]
-                )
-              }
-            )).to have_been_made.times(1)
+      expect(a_request(:post, 'https://api.telegram.org/botfake-token/sendMessage')
+              .with(body: {
+                      'chat_id' => '123',
+                      'text' => I18n.t(
+                        'bot.no_game',
+                        username: user_params[:username]
+                      )
+                    })).to have_been_made.times(1)
     end
   end
 
-  context "an active game exists" do
+  context 'an active game exists' do
     before do
       Game.create(chat_id: fake_chat_id, required_players: 5)
     end
 
-    scenario "new player joins game, a new player is created" do
-      message = Telegram::Bot::Types::Message.new(message_params('/set_time     06:00 '))
+    scenario 'set the time to next week' do
+      message = Telegram::Bot::Types::Message.new(message_params('/set_time next tuesday at 7pm'))
 
       pickup_bot.run(message)
 
-      expect(a_request(:post, "https://api.telegram.org/botfake-token/sendMessage").
-              with(body: {
-                "chat_id" => "123",
-                "text" => I18n.t(
-                  "bot.time",
-                  username: user_params[:username],
-                  time: "06:00"
-                )
-              }
-            )).to have_been_made.times(1)
-      expect(Game.last.time).to eq "06:00"
+      expect(a_request(:post, 'https://api.telegram.org/botfake-token/sendMessage')
+              .with(body: {
+                      'chat_id' => '123',
+                      'text' => "The date is set to #{next_tuesday} 19:00:00 +0200"
+                    })).to have_been_made.times(1)
     end
 
-    scenario "new player joins game, a new player is created" do
-      message = Telegram::Bot::Types::Message.new(message_params('/set_time next week'))
+    scenario 'set the time to 7pm' do
+      date = Date.today.to_s
+      message = Telegram::Bot::Types::Message.new(message_params('/set_time 7pm'))
 
       pickup_bot.run(message)
 
-      expect(a_request(:post, "https://api.telegram.org/botfake-token/sendMessage").
-              with(body: {
-                "chat_id" => "123",
-                "text" => I18n.t(
-                  "bot.time",
-                  username: user_params[:username],
-                  time: "06:00"
-                )
-              }
-            )).to have_been_made.times(1)
-      expect(Game.last.time).to eq "06:00"
+      expect(a_request(:post, 'https://api.telegram.org/botfake-token/sendMessage')
+              .with(body: {
+                      'chat_id' => '123',
+                      'text' => "The date is set to #{date} 19:00:00 +0200"
+                    })).to have_been_made.times(1)
+    end
+
+
+    def next_tuesday
+       Date.today
+         .then { |today| (1..7).find { |t| (today + t).tuesday? } }
+         .then { |days| today + days }
+         .then { |next_tuesday| next_tuesday.to_s }
+
     end
   end
 end
